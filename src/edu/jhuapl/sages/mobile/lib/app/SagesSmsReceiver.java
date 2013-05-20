d19e24979cc7fba2e489cd1e9aa2f06a3a77d7f7
pokuam1 <adjoa.poku@jhuapl.edu>
2013-05-20 15:29:56 -0400

    encryption/decryption + base64encode/decode via SMS with AES-128 bit key

---
+++ src/edu/jhuapl/sages/mobile/lib/app/SagesSmsReceiver.java
@@ -0,0 +1,192 @@
+package edu.jhuapl.sages.mobile.lib.app;
+
+import java.io.File;
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+
+import javax.crypto.NoSuchPaddingException;
+
+import org.spongycastle.util.encoders.Base64;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+import edu.jhuapl.sages.mobile.lib.app.crypto.ShowDecryptedMessageActivity;
+import edu.jhuapl.sages.mobile.lib.app.tests.crypto.SagesKeyTest;
+import edu.jhuapl.sages.mobile.lib.crypto.engines.CryptoEngine;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesKey.KeyEnum;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesKeyException;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesKeyStore;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesPrivateKey;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesPublicKey;
+import edu.jhuapl.sages.mobile.lib.message.DataMessage;
+import edu.jhuapl.sages.mobile.lib.message.KeyExchangeMessage;
+import edu.jhuapl.sages.mobile.lib.message.MessageBuilderUtil;
+import edu.jhuapl.sages.mobile.lib.message.SagesMessage;
+import edu.jhuapl.sages.mobile.lib.message.SagesMessage.MsgTypeEnum;
+
+/**
+ * TODO:
+ * - if exception occurs during key-exch, need to send back NACK
+ * - need to keep track of state of exchange model to ensure all steps occurred and in sync both sides
+ * 
+ * @author pokuam1
+ * @date May, 5 2013
+ */
+public class SagesSmsReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Bundle extras = intent.getExtras();
+		
+		if (extras == null) {
+			return;
+		}
+
+		Toast.makeText(context, "SMS Received", Toast.LENGTH_SHORT).show();
+		
+		Object[] pdus = (Object[])extras.get("pdus");
+		for (int i = 0; i < pdus.length; i++){
+			SmsMessage sms = SmsMessage.createFromPdu((byte[])pdus[i]);
+			String sender = sms.getOriginatingAddress();
+			String body = sms.getMessageBody();
+			
+			/**
+			 * if sms body contains the text for "key_exch", then process as key exchange message.
+			 *  -> extracts the public key
+			 *  -> saves public key for this sender to the keystore
+			 *  
+			 * if sms body contains the text for "data", and also "enc aes" then process as an encrypted msg
+			 */
+			boolean keyExchangeSuccess = false;
+			try {
+				if (body.contains(SagesMessage.data)){
+					DataMessage dataMsg = new DataMessage();
+					dataMsg.setSender(sender);
+					dataMsg.processRawMessage(body);
+					
+					if (body.contains("enc aes")){
+						DataMessage dataMessage = new DataMessage();
+						dataMessage.setSender(sender);
+						dataMessage.processRawMessage(body);
+						
+						//TODO: when production key store should live on internal device storage. during testing allows to have visibility.
+						File file = MainActivity.KEYSTORE_FILE;
+						SagesKeyStore keystore = new SagesKeyStore(file);
+						
+						SagesPrivateKey dummy_private_aes = (SagesPrivateKey) keystore.lookupKey("+12404759206", KeyEnum.PRIVATE);
+						try {
+							CryptoEngine crytpoengine = new CryptoEngine(dummy_private_aes.getData());
+							String b64Encoded = dataMsg.getBody().getBody();
+
+							byte[] b64EncodedCipher = (dataMsg.getBody().getBody().getBytes());
+							byte[] b64DecodedCipher = Base64.decode(b64EncodedCipher);
+							byte[] decryptedMsg = crytpoengine.decrypt(b64DecodedCipher);
+							String raw = new String(decryptedMsg);
+							
+							Intent intentDecrypt = new Intent(context, ShowDecryptedMessageActivity.class);
+							intentDecrypt.putExtra("msg_body", body);
+							intentDecrypt.putExtra("msg_body_decrypted", decryptedMsg);
+							intentDecrypt.putExtra("msg_sender", sender);
+							intentDecrypt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+							context.startActivity(intentDecrypt);
+							break;
+
+						} catch (NoSuchAlgorithmException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (NoSuchPaddingException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				else if (body.contains(SagesMessage.key_exch)){
+					//TODO: when production key store should live on internal device storage. during testing allows to have visibility.
+					File file = MainActivity.KEYSTORE_FILE;
+					SagesKeyStore keystore = new SagesKeyStore(file);
+
+					
+					if(body.contains(SagesMessage.init)){ 
+						Toast.makeText(context, "Key exchange initiated by " + sender, Toast.LENGTH_SHORT).show();
+						/* Just recieved a KeyExchange[init] from sender, so I will need:
+						 * 1) extract sender's key and save key to my keystore
+						 * 2) return my own keys back to the sender by sending a KeyExchange[reply] as SMS
+						 *    2a) also, if i don't have my keys stored, save them (intend to only encounter this in testing)
+						 */
+						
+						if (body.contains("aes")){ //TODO this is for quick test only
+							KeyExchangeMessage keyExch = new KeyExchangeMessage();
+							keyExch.setSender(sender);
+							keyExch.processRawMessage(body);
+							SagesPrivateKey privKey = (SagesPrivateKey) keyExch.getKey();
+							
+							keystore.addKey(privKey);
+							
+							Intent intentDecrypt = new Intent(context, ShowDecryptedMessageActivity.class);
+							intentDecrypt.putExtra("msg_body", body);
+							intentDecrypt.putExtra("msg_sender", sender);
+							intentDecrypt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+							context.startActivity(intentDecrypt);
+							break;
+						} else {
+						//1)
+						KeyExchangeMessage keyExch = new KeyExchangeMessage();
+						keyExch.setSender(sender);
+						keyExch.processRawMessage(body);
+						SagesPublicKey pubKey = (SagesPublicKey) keyExch.getKey();
+						
+						keystore.addKey(pubKey);
+						}
+						//2)
+						
+						String msgHeader = MessageBuilderUtil.genTestHeader(MsgTypeEnum.KEY_EXCH, "reply");
+						String msgBody = SagesMessage.my_key + SagesKeyTest.testData + "- for key:" + MainActivity.NUMBER_THIS_DEVICE + " - " + Calendar.MILLISECOND;
+						try {
+							MainActivity.saveOwnKeyPriorToSMS(context, "+" + MainActivity.NUMBER_THIS_DEVICE, (msgHeader + msgBody));
+						} catch (SagesKeyException e1){
+							Toast.makeText(context, "Unable to save own key. Key exchange reply will not be sent to " + sender, Toast.LENGTH_SHORT).show();
+						}
+						
+						SmsManager smsManager = SmsManager.getDefault();
+						smsManager.sendTextMessage(sender, null, msgHeader + msgBody, null, null);
+						
+					} else if (body.contains(SagesMessage.reply)){
+						Toast.makeText(context, "Key exchange reply from " + sender, Toast.LENGTH_SHORT).show();
+						/* This has the remote key from the device I requested key for, and it needs to be saved to my keystore 
+						 * 1) save key for the remote sender
+						 * 
+						 */
+						KeyExchangeMessage keyExch = new KeyExchangeMessage();
+						keyExch.setSender(sender);
+						keyExch.processRawMessage(body);
+						SagesPublicKey pubKey = (SagesPublicKey) keyExch.getKey();
+						
+						keystore.addKey(pubKey);
+					}
+					
+					Toast.makeText(context, "Public key stored for sender " + sender, Toast.LENGTH_SHORT).show();
+				}
+				
+				/**
+				 * Now time to respond by sending own(aka 'remote' to remove conufsion) public key back to the initiator
+				 * TOOD: break this into another process?
+				 */
+				
+			} catch (SagesKeyException e) {
+				e.printStackTrace();
+				// SEND NACK ON KEY EXCHANGE
+					// TODO
+			}
+			}
+		
+			
+		}
+	}
---
+++ src/edu/jhuapl/sages/mobile/lib/app/crypto/KeyGenerateActivity.java
@@ -0,0 +1,219 @@
+package edu.jhuapl.sages.mobile.lib.app.crypto;
+
+import java.io.File;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.NoSuchPaddingException;
+
+import org.spongycastle.util.encoders.Base64;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import edu.jhuapl.sages.mobile.lib.app.R;
+import edu.jhuapl.sages.mobile.lib.crypto.engines.CryptoEngine;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesKeyException;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesKeyStore;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesPrivateKey;
+import edu.jhuapl.sages.mobile.lib.message.MessageBuilderUtil;
+import edu.jhuapl.sages.mobile.lib.message.SagesMessage;
+import edu.jhuapl.sages.mobile.lib.message.SagesMessage.MsgTypeEnum;
+
+/**
+ * Demonstrates generation of a true cryptographic key (AES-128 bit for now), and the results of 
+ * encrypting, base64 encoding and base64 decoding, decrypting a hard coded string. The key that is generated 
+ * contains a pseudo-random suffix to demonstrate correct functionality.
+ * 
+ * @author pokuam1
+ * @date 5/16/2013
+ */
+public class KeyGenerateActivity extends Activity {
+
+	private static final String DUMMY_MESSAGE_CLEAR = "This is the test message to encrypt and decrypt. Over and over.";
+	private String seedpwd = "DUMMYPASSWORD1";
+	private String pwd = "DUMMYPASSWORD1";
+	private int a;
+	private int b;
+	
+	private TextView key;
+	private TextView msg;
+	private TextView msgEncrypted;
+	private TextView msgDecrypted;
+	private CryptoEngine crypto;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_keygen);
+		
+		makeNewPassword();
+		try {
+			
+			crypto = new CryptoEngine((pwd).getBytes());
+			key = (TextView)findViewById(R.id.txt_privateKey);
+			key.setText("KEY ENCODED: "  + pwd + "-" + crypto.getSkeySpec().getEncoded().toString());
+			
+			msg = (TextView)findViewById(R.id.txt_messageclear);
+			msg.setText(DUMMY_MESSAGE_CLEAR);
+			
+			byte[] encryptedBytes = crypto.encrypt(DUMMY_MESSAGE_CLEAR.getBytes());
+			msgEncrypted = (TextView)findViewById(R.id.txt_msg_encrypted);
+			msgEncrypted.setText("Encrypted: " + encryptedBytes.toString());
+			
+			
+			byte[] decryptedBytes = crypto.decrypt(encryptedBytes);
+			msgDecrypted = (TextView)findViewById(R.id.txt_msg_decrypted);
+			msgDecrypted.setText("Decrypted: " + new String(decryptedBytes));
+			
+			TelephonyManager tMgr =  (TelephonyManager)this.getSystemService(TELEPHONY_SERVICE);
+			final String phoneNum = tMgr.getLine1Number();
+			
+			final SagesKeyStore keystore = new SagesKeyStore(new File(Environment.getExternalStorageDirectory() + "/sageslib_testout/"));
+
+			Button genButton = (Button)findViewById(R.id.btn_genkey);
+			genButton.setOnClickListener(new OnClickListener() {
+				
+				
+				@Override
+				public void onClick(View v) {
+					makeNewPassword();
+					demonstrateEncryptionDecryption();
+					SagesPrivateKey privateKey = new SagesPrivateKey(KeyGenerateActivity.this, "+" + phoneNum, null, crypto.getSkeySpec().getEncoded());
+					try {
+						keystore.addKey(privateKey);
+						Toast.makeText(KeyGenerateActivity.this, "Private key saved for " + phoneNum, Toast.LENGTH_LONG).show();
+					} catch (SagesKeyException e) {
+						e.printStackTrace();
+						Toast.makeText(KeyGenerateActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+					}
+				}
+			});
+			
+			
+			Button sendKeyButton = (Button)findViewById(R.id.btn_send_aes_key);
+			sendKeyButton.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					SmsManager smsMgr = SmsManager.getDefault();
+					
+					String msgHeader = MessageBuilderUtil.genTestHeader(MsgTypeEnum.KEY_EXCH, "init aes");
+					String msgBody = SagesMessage.my_key + new String(crypto.getSkeySpec().getEncoded());
+					String text = msgHeader + msgBody;
+					smsMgr.sendTextMessage("2404759981", null, text, null, null);
+					
+					
+				}
+			});
+			
+			Button encryptAndSendMsgButton = (Button)findViewById(R.id.btn_ecnrypt_send_msg);
+			encryptAndSendMsgButton.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					try {
+					SmsManager smsMgr = SmsManager.getDefault();
+					String msgHeader = MessageBuilderUtil.genTestHeader(MsgTypeEnum.DATA, "enc aes");
+					String msgBody;
+					byte[] cipheredBody = crypto.encrypt(KeyGenerateActivity.DUMMY_MESSAGE_CLEAR.getBytes());
+					
+					byte[] cipheredBodyB64 = Base64.encode(cipheredBody);
+						msgBody = new String(cipheredBodyB64);
+						String text = msgHeader + msgBody;
+						Toast.makeText(KeyGenerateActivity.this, "SMS length: " + text.length(), Toast.LENGTH_SHORT).show();
+						smsMgr.sendTextMessage("2404759981", null, text, null, null);
+//						smsMgr.sendDataMessage("2404759981", null, (Short) null, textBytes, null, null);
+						
+//						smsMgr.sendTextMessage("2404759981", null, "just a test", null, null);
+
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void makeNewPassword() {
+		int min = 0;
+		int max = 9;
+		
+		a = min + (int)(Math.random() * ((max - min) + 1));
+		b = min + (int)(Math.random() * ((max - min) + 1));
+		
+		// password has to be 16 bytes (i.e. 128 bits)
+		pwd = seedpwd + a + b;
+	}
+	
+	private void demonstrateEncryptionDecryption(){
+		try {
+/*			crypto = new CryptoEngine((pwd).getBytes());
+			
+			key = (TextView)findViewById(R.id.txt_privateKey);
+			key.setText("KEY ENCODED: " + pwd + "-" + crypto.getSkeySpec().getEncoded().toString());
+			
+			msg = (TextView)findViewById(R.id.txt_messageclear);
+			msg.setText(DUMMY_MESSAGE_CLEAR);
+			
+			byte[] encryptedBytes = crypto.encrypt(DUMMY_MESSAGE_CLEAR.getBytes());
+			msgEncrypted = (TextView)findViewById(R.id.txt_msg_encrypted);
+			msgEncrypted.setText("Encrypted: " + encryptedBytes.toString());
+			
+			
+			byte[] decryptedBytes = crypto.decrypt(encryptedBytes);
+			msgDecrypted = (TextView)findViewById(R.id.txt_msg_decrypted);
+			msgDecrypted.setText("Decrypted: " + new String(decryptedBytes));
+*/			
+			crypto = new CryptoEngine((pwd).getBytes());
+			
+			key = (TextView)findViewById(R.id.txt_privateKey);
+			key.setText("KEY ENCODED: " + pwd + "-" + crypto.getSkeySpec().getEncoded().toString());
+			
+			msg = (TextView)findViewById(R.id.txt_messageclear);
+			msg.setText(DUMMY_MESSAGE_CLEAR);
+			
+			byte[] encryptedBytes = crypto.encrypt(DUMMY_MESSAGE_CLEAR.getBytes());
+			byte[] b64Cipher = Base64.encode(encryptedBytes);
+			msgEncrypted = (TextView)findViewById(R.id.txt_msg_encrypted);
+			msgEncrypted.setText("Encrypted: " + new String(encryptedBytes) + " Encoded b64: " + new String(b64Cipher));
+			
+			byte[] b64DecodedCipher = Base64.decode(new String(b64Cipher));
+			byte[] decryptedBytes = crypto.decrypt(b64DecodedCipher);
+//			byte[] decryptedBytes = crypto.decrypt(encryptedBytes);
+			msgDecrypted = (TextView)findViewById(R.id.txt_msg_decrypted);
+			msgDecrypted.setText("b64 Decoded: " + new String(b64DecodedCipher) + " Decrypted: " + new String(decryptedBytes));
+			
+					
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
