44ca1342e1f800cd546baaaa8faec2c6a8082826
pokuam1 <adjoa.poku@jhuapl.edu>
2013-05-29 10:53:59 -0400

    initialize null cryptoengine with hardcoded AES-128 bit key

--- src/edu/jhuapl/sages/mobile/lib/SharedObjects.java
+++ src/edu/jhuapl/sages/mobile/lib/SharedObjects.java
@@ -4,7 +4,10 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.NoSuchPaddingException;
 
+import android.util.Log;
+
 import edu.jhuapl.sages.mobile.lib.crypto.engines.CryptoEngine;
+import edu.jhuapl.sages.mobile.lib.crypto.persisted.SagesKeyException;
 
 public class SharedObjects {
 	
@@ -22,11 +25,15 @@ public class SharedObjects {
 	 */
 	public static CryptoEngine getCryptoEngine() throws NoSuchAlgorithmException, NoSuchPaddingException {
 		if (cryptoEngine == null){
-			SharedObjects so = new SharedObjects();
+			//TODO need to come up with long term solution to handle this. leave null and throw exception?
+			cryptoEngine = new CryptoEngine("PASSWORD12345678".getBytes());
 		}
 		return cryptoEngine;
 	}
 	
-	
+	protected static void updateCryptoEngine(String keyVal) throws NoSuchAlgorithmException, NoSuchPaddingException{
+		cryptoEngine = new CryptoEngine(keyVal.getBytes());
+		Log.i(SharedObjects.class.getName(), "updated CryptoEnginge.");
+	}
 
 }
