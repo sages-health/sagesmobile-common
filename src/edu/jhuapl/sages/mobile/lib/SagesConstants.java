40971bd561d1769823ecbae5c9527ef66cccd55a
pokuam1 <adjoa.poku@jhuapl.edu>
2013-06-24 12:51:20 -0400

    system wide constants

---
+++ src/edu/jhuapl/sages/mobile/lib/SagesConstants.java
@@ -0,0 +1,12 @@
+package edu.jhuapl.sages.mobile.lib;
+
+import java.io.File;
+
+import android.os.Environment;
+
+public class SagesConstants {
+
+	public static final File KEYSTORE_FILE = new File(Environment.getExternalStorageDirectory().getAbsoluteFile() + "/sageslib_testout");
+	
+	
+}
